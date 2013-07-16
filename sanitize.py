aa742a8c669f12fdc26736c181bb7cc955ee9671
bluquar <chrisbarker@cmu.edu>
2013-07-16 09:24:25 -0700

    remove sanitize.py
    Move sanitize function to scrape.py

--- sanitize.py
+++
@@ -1,7 +0,0 @@
-__doc__ = """Method for sanitizing strings to get safe filenames."""
-
-import string
-valid_chars = frozenset(''.join(("-_.() ", string.ascii_letters, string.digits)))
-
-def sanitize(s):
-    return ''.join(c for c in s if c in valid_chars)
